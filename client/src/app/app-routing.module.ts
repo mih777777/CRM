2b1e4e54488d242eea4ce420cf6848cc50894f58
mih777 <mih777777@yandex.ru>
2020-02-25 18:13:56 +0300

    еще лэйоуты подредактировал и теперь они готовы

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { AuthLayoutComponent } from './shared/layouts/auth-layout/auth-layout.component';
+import { RegisterPageComponent } from './register-page/register-page.component';
 
 
 const routes: Routes = [
   {
     path: '', component: AuthLayoutComponent, children: [
-      {path: 'login', component: LoginPageComponent}
+      {path: 'login', component: LoginPageComponent},
+      {path: 'register', component: RegisterPageComponent}
     ]
   }
 ]
