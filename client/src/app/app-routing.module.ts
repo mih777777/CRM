ca00f4e6c07b972a5fa59f4defe95292ae4e33db
mih777 <mih777777@yandex.ru>
2020-02-25 18:03:34 +0300

    отредактировал  меню логина

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { AuthLayoutComponent } from './shared/layouts/auth-layout/auth-layout.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: AuthLayoutComponent, children: [
+      {path: 'login', component: LoginPageComponent}
+    ]
+  }
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { RegisterPageComponent } from './register-page/register-page.component';
+import { AuthLayoutComponent } from './shared/layouts/auth-layout/auth-layout.component';
+import { SiteLayoutComponent } from './shared/layouts/site-layout/site-layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent,
+    RegisterPageComponent,
+    AuthLayoutComponent,
+    SiteLayoutComponent
   ],
   imports: [
     BrowserModule,
