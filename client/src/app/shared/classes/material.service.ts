0ee555b89928b0b1055bc4077c6e8a97c6c4b33b
mih777 <mih777777@yandex.ru>
2020-03-04 22:34:49 +0300

    site-layout готов, кнопа работает

--- client/src/app/shared/classes/material.service.ts
+++ client/src/app/shared/classes/material.service.ts
@@ -1,7 +1,13 @@
+import { ElementRef } from '@angular/core'
+
 declare let M: any
 
 export class MaterialService {
     static toast(message: string) {
         M.toast({html: message})
+    }
+
+    static initializeFloatingButton(ref: ElementRef) {
+        M.FloatingActionButton.init(ref.nativeElement)
     }
 }
--- client/src/app/shared/layouts/site-layout/site-layout.component.ts
+++ client/src/app/shared/layouts/site-layout/site-layout.component.ts
@@ -1,11 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component,  AfterViewInit, ViewChild, ElementRef } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
+import { MaterialService } from '../../classes/material.service';
 
 @Component({
   selector: 'app-site-layout',
   templateUrl: './site-layout.component.html',
   styleUrls: ['./site-layout.component.scss']
 })
-export class SiteLayoutComponent implements OnInit {
+export class SiteLayoutComponent implements AfterViewInit {
+
+  @ViewChild('floating') floatingRef: ElementRef
 
   links = [
     {url: '/overview', name: 'Обзор'},
@@ -15,9 +20,19 @@ export class SiteLayoutComponent implements OnInit {
     {url: '/categories', name: 'Ассортимент'},
   ]
 
-  constructor() { }
+  constructor(
+    private auth: AuthService,
+    private router: Router
+  ) { }
 
-  ngOnInit(): void {
+  ngAfterViewInit() {
+    MaterialService.initializeFloatingButton(this.floatingRef)
+  }
+
+  logout(event: Event) {
+    event.preventDefault()
+    this.auth.logout()
+    this.router.navigate(['/login'])
   }
 
 }
